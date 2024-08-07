4ebed11aea229406c05a06198bd216e9a7ab0eb5
Abdulaziz Ergashev <eabdulaziz36@gmail.com>
2024-08-07 11:32:27 +0500

    JS For Loop

---
+++ script.js
@@ -0,0 +1,74 @@
+// TASK 1 ||||||||||||||||||||||||||||| Yozuv ichidagi sonlarni yeg'indisini chiqarish 
+
+
+// let id = 'a2b9d4u6l3a5z8i7z'; // id has length of 17 and its indexes are (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+// let sum = 0;
+
+// for (let i = 0; i < id.length; i++) {
+//     if (!isNaN(id[i])) {
+//         sum += Number(id[i]);
+//     }
+// }
+// console.log(sum);
+
+
+// TASK 2 ||||||||||||||||||||||||||||| Yozuvlarni aloxida raqamlarni aloxida qilish
+
+// let id = "a2b9d4u6l3a5z8i7z"; // id has length of 17 and its indexes are (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
+// let str = '';
+// let num = '';
+
+// for (let i = 0; i < id.length; i++) {
+//     if (isNaN(id[i])) {
+//         str += id[i];
+//     } else {
+//         num += id[i];
+//     }
+// }
+// console.log(str);
+// console.log(num);
+
+
+
+// TASK 2 ||||||||||||||||||||||||||||| Yozuv ichidagi juft raqamlarni topish 
+
+// let id = "a2b9d4u6l3a5z8i7z";
+// let num = '';
+
+// for (let i = 0; i < id.length; i++){
+//     if (!isNaN(id[i])){
+//         if (id[i] % 2 == 0){
+//             num += id[i];
+//         }
+//     }
+// }
+
+// console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ifga oid malalar ishlash
